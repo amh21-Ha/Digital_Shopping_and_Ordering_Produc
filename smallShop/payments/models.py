adefb0b521cb94efd9901cfecef714decb21e413
Amha <amha.ha07@gmail.com>
2024-06-27 15:06:26 +0300

    small shops

---
+++ smallShop/payments/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+
+class Payment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Payment {self.id} by {self.user.username}"
---
+++ smallShop/users/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
